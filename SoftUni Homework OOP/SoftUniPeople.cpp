95eaa6d3631cef9a9132ddd7fd4f0fc9596b05d8
Jimbo <Jimbo@MALATAR-PC>
2016-06-14 19:58:32 +0300

    Add project files.

---
+++ SoftUni Homework OOP/SoftUniPeople.cpp
@@ -0,0 +1,8 @@
+#include "SoftUniPeople.h"
+short SoftUniPeople::id = 0;
+
+
+std::string SoftUniPeople::getName()
+{
+	return name;
+}
---
+++ SoftUni Homework OOP/Students.cpp
@@ -0,0 +1,6 @@
+#include "Students.h"
+
+short Students::getId()
+{
+	return id;
+}
